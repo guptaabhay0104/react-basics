e8fc45299b35a7000932c41e43fcf3f69d1b6382
guptaabhay0104 <abhayg1804@gmail.com>
2024-07-17 01:24:53 +0530

    React Router Essentials

---
+++ 08reactRouter/src/components/User/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+
+
+function User() {
+
+  const {userid} = useParams()
+
+  return (
+    <div className='bg-gray-600 text-white p-3 text-center'>User: {userid}</div>
+  )
+}
+
+export default User
